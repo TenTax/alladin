98b2ef4a6e5a642a2a61077aa0be87e3c42ce678
TenTax <Kostya.dorokhov.2016@mail.ru>
2020-09-12 18:13:11 +0300

    Изменения в обратном отсчете

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,81 +1,92 @@
-"use strict";
+var ROOT_TIME = document.getElementById('time');
+var ROOT_MODAL = document.getElementById('modal');
 
-var ROOT_TIME = document.getElementById("time");
-var ROOT_MODAL = document.getElementById("modal");
+function renderTime (days, hours, minutes, secounds) {
+    ROOT_TIME.innerHTML = `
+    <div class="timeout__item">
+        <div class="timeout__booble">${ days }</div>
+        <div class="timeout__subbooble">Дней</div>
+    </div>
+    <div class="timeout__item">
+        <div class="timeout__booble">${ hours }</div>
+        <div class="timeout__subbooble">Часов</div>
+    </div>
+    <div class="timeout__item">
+        <div class="timeout__booble">${ minutes }</div>
+        <div class="timeout__subbooble">Минут</div>
+    </div>
+    <div class="timeout__item">
+        <div class="timeout__booble">${ secounds }</div>
+        <div class="timeout__subbooble">Секунд</div>
+    </div>
+    `;
+}
 
-var renderTime = function renderTime(days, hours, minutes, secounds) {
-  ROOT_TIME.innerHTML = '\n    <div class="timeout__item">\n        <div class="timeout__booble">'
-    .concat(
-      days,
-      '</div>\n        <div class="timeout__subbooble">\u0414\u043D\u0435\u0439</div>\n    </div>\n    <div class="timeout__item">\n        <div class="timeout__booble">'
-    )
-    .concat(
-      hours,
-      '</div>\n        <div class="timeout__subbooble">\u0427\u0430\u0441\u043E\u0432</div>\n    </div>\n    <div class="timeout__item">\n        <div class="timeout__booble">'
-    )
-    .concat(
-      minutes,
-      '</div>\n        <div class="timeout__subbooble">\u041C\u0438\u043D\u0443\u0442</div>\n    </div>\n    <div class="timeout__item">\n        <div class="timeout__booble">'
-    )
-    .concat(
-      secounds,
-      '</div>\n        <div class="timeout__subbooble">\u0421\u0435\u043A\u0443\u043D\u0434</div>\n    </div>\n    '
-    );
-};
+setInterval(function() {
+    var date = new Date();
+    renderTime(date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds());
+});
 
-setInterval(function () {
-  var date = new Date();
-  renderTime(
-    date.getDate(),
-    date.getHours(),
-    date.getMinutes(),
-    date.getSeconds()
-  );
-}, 1000);
+function GalleryComponent() {
+    return (`
+        <div class="gallery-modal">
+            <div class="gallery-modal__item">
+                <img src="http://lp3-stylish-curtains.oml.ru/thumb/2/ytXolYNW_lZGN5gTgx711A/r556/d/54687699_2.jpg" alt=""  class="gallery-modal__img">
+            </div>
+            <div class="gallery-modal__item">
+                <img src="http://lp3-stylish-curtains.oml.ru/thumb/2/AyDBgNBY_WwzcS4JxYLvUg/r556/d/54580154_2.jpg" alt=""  class="gallery-modal__img">
+            </div>
+            <div class="gallery-modal__item">
+                <img src="http://lp3-stylish-curtains.oml.ru/thumb/2/cnbB2fFZdjv-WemZQ441Ng/r556/d/54159261_2.jpg" alt=""  class="gallery-modal__img">
+            </div>
+            <div class="gallery-modal__item">
+                <img src="http://lp3-stylish-curtains.oml.ru/thumb/2/ytXolYNW_lZGN5gTgx711A/r556/d/54687699_2.jpg" alt=""  class="gallery-modal__img">
+            </div>
+            <div class="gallery-modal__item">
+                <img src="http://lp3-stylish-curtains.oml.ru/thumb/2/ytXolYNW_lZGN5gTgx711A/r556/d/54687699_2.jpg" alt=""  class="gallery-modal__img">
+            </div>
+        </div>
+    `);
+}
 
-var GalleryComponent = function GalleryComponent() {
-  return '\n        <div class="gallery-modal">\n            <div class="gallery-modal__item">\n                <img src="http://lp3-stylish-curtains.oml.ru/thumb/2/ytXolYNW_lZGN5gTgx711A/r556/d/54687699_2.jpg" alt=""  class="gallery-modal__img">\n            </div>\n            <div class="gallery-modal__item">\n                <img src="http://lp3-stylish-curtains.oml.ru/thumb/2/AyDBgNBY_WwzcS4JxYLvUg/r556/d/54580154_2.jpg" alt=""  class="gallery-modal__img">\n            </div>\n            <div class="gallery-modal__item">\n                <img src="http://lp3-stylish-curtains.oml.ru/thumb/2/cnbB2fFZdjv-WemZQ441Ng/r556/d/54159261_2.jpg" alt=""  class="gallery-modal__img">\n            </div>\n            <div class="gallery-modal__item">\n                <img src="http://lp3-stylish-curtains.oml.ru/thumb/2/ytXolYNW_lZGN5gTgx711A/r556/d/54687699_2.jpg" alt=""  class="gallery-modal__img">\n            </div>\n            <div class="gallery-modal__item">\n                <img src="http://lp3-stylish-curtains.oml.ru/thumb/2/ytXolYNW_lZGN5gTgx711A/r556/d/54687699_2.jpg" alt=""  class="gallery-modal__img">\n            </div>\n        </div>\n    ';
-};
+function renderModal(param, id) {
+    ROOT_MODAL.innerHTML = `
+        <div class="modal__root">
+            <div class="modal__close" onclick="closeModal()"><i class="fas fa-times"></i></div>
+            <div class="modal__content">
+                ${
+                    param == 'gallery'
+                    ? GalleryComponent()
+                    : 'nobody'
+                }
+            </div>
+        </div>
+    `;
 
-var renderModal = function renderModal(param, id) {
-  ROOT_MODAL.innerHTML = '\n        <div class="modal__root">\n            <div class="modal__close" onclick="closeModal()"><i class="fas fa-times"></i></div>\n            <div class="modal__content">\n                '.concat(
-    param == "gallery" ? GalleryComponent() : "nobody",
-    "\n            </div>\n        </div>\n    "
-  );
-  $(".gallery-modal").slick({
-    prevArrow:
-      '<div class="gallery-modal__prev"><i class="fas fa-chevron-left"></i></div>',
-    nextArrow:
-      '<div class="gallery-modal__next"><i class="fas fa-chevron-right"></i></div>',
-    centerMode: true,
-    dots: false,
-    arrows: true,
-    centerPadding: "0px",
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    initialSlide: id,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false
-        }
-      }
-    ]
-  });
-};
+    $('.gallery-modal').slick({
+        prevArrow: '<div class="gallery-modal__prev"><i class="fas fa-chevron-left"></i></div>',
+        nextArrow: '<div class="gallery-modal__next"><i class="fas fa-chevron-right"></i></div>',
+        centerMode: true,
+        dots: false,
+        arrows: true,
+        centerPadding: '0px',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: id,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
+    });
+}
 
-var closeModal = function closeModal() {
-  ROOT_MODAL.innerHTML = "";
-};
-
-
-
-
-
-
-
-
+function closeModal() {
+    ROOT_MODAL.innerHTML = '';
+}
 
 
 
