475a61aa8de785f737b39ea74e7afd0af50f4f9e
TenTax <Kostya.dorokhov.2016@mail.ru>
2020-09-12 19:20:51 +0300

    Доделал таймер

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,31 +1,86 @@
 var ROOT_TIME = document.getElementById('time');
 var ROOT_MODAL = document.getElementById('modal');
 
-function renderTime (days, hours, minutes, secounds) {
+function renderTime ({ days, hours, minutes, seconds }) {
     ROOT_TIME.innerHTML = `
     <div class="timeout__item">
         <div class="timeout__booble">${ days }</div>
-        <div class="timeout__subbooble">Дней</div>
+        <div class="timeout__subbooble">${ padDate(days, 'days') }</div>
     </div>
     <div class="timeout__item">
         <div class="timeout__booble">${ hours }</div>
-        <div class="timeout__subbooble">Часов</div>
+        <div class="timeout__subbooble">${ padDate(hours, 'hours') }</div>
     </div>
     <div class="timeout__item">
         <div class="timeout__booble">${ minutes }</div>
-        <div class="timeout__subbooble">Минут</div>
+        <div class="timeout__subbooble">${ padDate(minutes, 'minutes') }</div>
     </div>
     <div class="timeout__item">
-        <div class="timeout__booble">${ secounds }</div>
-        <div class="timeout__subbooble">Секунд</div>
+        <div class="timeout__booble">${ seconds }</div>
+        <div class="timeout__subbooble">${ padDate(seconds, 'seconds') }</div>
     </div>
     `;
 }
 
-setInterval(function() {
-    var date = new Date();
-    renderTime(date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds());
-});
+var target_date = new Date(2020, 8, 29).getTime();
+var days, hours, minutes, seconds; // переменные для единиц времени
+ 
+setInterval(function () { renderTime(getCountdown()) }, 1000);
+ 
+function getCountdown(){
+ 
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+ 
+    days = pad( parseInt(seconds_left / 86400) );
+    seconds_left = seconds_left % 86400;
+          
+    hours = pad( parseInt(seconds_left / 3600) );
+    seconds_left = seconds_left % 3600;
+           
+    minutes = pad( parseInt(seconds_left / 60) );
+    seconds = pad( parseInt( seconds_left % 60 ) );
+ 
+    return {days, hours, minutes, seconds};
+}
+ 
+function pad(n) {
+    return (n < 10 ? '0' : '') + n;
+}
+
+function padDate(n, param) {
+    params = {
+        days: [ 'дней', 'дня', 'день' ],
+        hours: [ 'часов', 'часа', 'час' ],
+        minutes: [ 'минут', 'минуты', 'минут' ],
+        seconds: [ 'секунд', 'секунды', 'секунд' ]
+    };
+
+    switch(param) {
+        case 'days':
+            var currentParam = params.days;
+            break;
+        case 'hours':
+            var currentParam = params.hours;
+            break;
+        case 'minutes':
+            var currentParam = params.minutes;
+            break;
+        case 'seconds':
+            var currentParam = params.seconds;
+            break;
+        default:
+            var currentParam = ['', '', ''];
+    }
+    
+    if ((n % 10 >= 5 && n % 10 <= 9) || n % 10 == 0 || (n >= 11 && n <=19)) return currentParam[0];
+    else if (n % 10 >= 2 && n % 10 <= 4) return currentParam[1];
+    else if(n == 1 || n % 10 == 1) return currentParam[2];
+}
+
+
+
+
 
 function GalleryComponent() {
     return (`
