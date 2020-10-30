f32fa75119bc058ac84158b8608ca85c86ca91e0
TenTax <Kostya.dorokhov.2016@mail.ru>
2020-10-30 22:58:33 +0300

    Добавлен Gallery на slick-carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
-import './slider'
+import $ from 'jquery';
+import 'slick-carousel';
+
+import slickGallery from './slick-sliders/slickGallery';
 
 window.addEventListener('DOMContentLoaded', () => {
-   
+   slickGallery($);
 });
