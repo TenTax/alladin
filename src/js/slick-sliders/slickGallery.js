11cabbeb3ba5f15f5ac5871dab45fc249f92c958
TenTax <Kostya.dorokhov.2016@mail.ru>
2020-10-31 11:35:39 +0300

    Окончательный рефакториг Gallery

--- src/js/slick-sliders/slickGallery.js
+++ src/js/slick-sliders/slickGallery.js
@@ -2,40 +2,23 @@ const sliderGallery = ($) => {
     $('.gallery__slider').slick({
         adaptiveHeight: true,
         slidesToShow: 3,
-        centerMode: true,
-        centerPadding: '150px',
         speed: 300,
+        autoplay: true,
+        autoplaySpeed: 3000,
         dots: true,
         appendArrows: $('.gallery__arrows'),
         appendDots: $('.gallery__dots'),
         responsive: [
             {
-                breakpoint: 1200,
-                settings: {
-                    centerPadding: '80px'
-                }
-            },
-            {
                 breakpoint: 992,
                 settings: {
-                    centerPadding: '40px'
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    centerPadding: '60px',
-                    slidesToShow: 2,
-                    centerMode: false
+                    slidesToShow: 2
                 }
             },
             {
                 breakpoint: 576,
                 settings: {
-                    centerPadding: '40px',
-                    slidesToShow: 1,
-                    arrows: false,
-                    dots: false
+                    slidesToShow: 1
                 }
             }
         ]
