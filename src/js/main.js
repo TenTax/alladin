c5a7559e1fdcdd493de8f17c537a734c4d7c30a5
TenTax <Kostya.dorokhov.2016@mail.ru>
2020-11-01 20:25:25 +0300

    Рефакторинг Starter + timer.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 import $ from 'jquery';
 import 'slick-carousel';
+import timer from './modules/timer';
 import slickGallery from './slick-sliders/slickGallery';
 import slickGalleryModal from './slick-sliders/slickGalleryModal';
 import slickPartners from './slick-sliders/slickPartners';
 import slickTestimonials from './slick-sliders/slickTestimonials';
 
 window.addEventListener('DOMContentLoaded', () => {
+   timer('#timer', '2020-11-03');
    slickGallery($);
    slickGalleryModal($);
    slickPartners($);
