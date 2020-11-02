926b720feab4dde7ed676c0fd791cd76b34c1d7d
TenTax <Kostya.dorokhov.2016@mail.ru>
2020-11-02 16:25:29 +0300

    Переверстал блок Catalog

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ import slickPartners from './slick-sliders/slickPartners';
 import slickTestimonials from './slick-sliders/slickTestimonials';
 
 window.addEventListener('DOMContentLoaded', () => {
-   timer('#timer', '2020-11-03');
+   timer('#timer', '2021-11-03');
    slickGallery($);
    slickGalleryModal($);
    slickPartners($);
