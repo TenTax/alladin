303d886c8782d55a1bc68c16182f3328d6822971
TenTax <Kostya.dorokhov.2016@mail.ru>
2020-11-03 23:40:54 +0300

    version 2.0

---
+++ send.php
@@ -0,0 +1,60 @@
+<?php
+
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+
+$title = "Заголовок письма";
+$body = "
+<h2>Новое письмо</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br>
+";
+
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'test@mail.ru'; // Логин на почте
+    $mail->Password   = 'secret'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('test@mail.ru', 'Константин'); // Адрес самой почты и имя отправителя
+
+    $mail->addAddress('test@gmail.com'); // Получатель письма
+
+// Файл
+// if (!empty($file['name'][0])) {
+//     for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+//         $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+//         $filename = $file['name'][$ct];
+//         if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
+//             $mail->addAttachment($uploadfile, $filename);
+//             $rfile[] = "Файл $filename прикреплён";
+//         } else {
+//             $rfile[] = "Не удалось прикрепить файл $filename";
+//         }
+//     }   
+// }
+
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+echo json_encode(["result" => $result]); // Файл "resultfile" => $rfile // "status" => $status
