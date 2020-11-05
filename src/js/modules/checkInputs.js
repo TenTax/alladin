ff82850665c4892c5e4185f403687ddcae6c44b0
TenTax <Kostya.dorokhov.2016@mail.ru>
2020-11-05 19:10:26 +0300

    Рефакторинг кода

--- src/js/modules/checkInputs.js
+++ src/js/modules/checkInputs.js
@@ -1,43 +1,43 @@
 const checkInputs = (form) => {
-        const name = form.querySelector('[name="name"]'),
-            phone = form.querySelector('[name="phone"]'),
-            checkbox = form.querySelector('[type="checkbox"]');
+    const name = form.querySelector('[name="name"]'),
+        phone = form.querySelector('[name="phone"]'),
+        checkbox = form.querySelector('[type="checkbox"]');
 
-        const nameValue = name.value.trim(),
-            phoneValue = phone.value.trim();
+    const nameValue = name.value.trim(),
+        phoneValue = phone.value.trim();
 
-        let check = true;
+    let check = true;
 
 
-        if(nameValue == '') {
-            setErrorFor(name, 'Это поле не может быть пустым');
-        } else {
-            setSuccessFor(name);
-        }
+    if (nameValue == '') {
+        setErrorFor(name, 'Это поле не может быть пустым');
+    } else {
+        setSuccessFor(name);
+    }
 
-        if(phoneValue == '') {
-            setErrorFor(phone, 'Это поле не может быть пустым');
-        } else {
-            setSuccessFor(phone);
-        }
+    if (phoneValue == '') {
+        setErrorFor(phone, 'Это поле не может быть пустым');
+    } else {
+        setSuccessFor(phone);
+    }
 
-        if(!checkbox.checked) {
-            setErrorFor(checkbox, 'Установите флажок');
-        } else {
-            checkbox.parentNode.className = 'form__group';
-        }
+    if (!checkbox.checked) {
+        setErrorFor(checkbox, 'Установите флажок');
+    } else {
+        checkbox.parentNode.className = 'form__group';
+    }
 
-        function setErrorFor(input, message) {
-            check = false;
-            input.parentNode.querySelector('.form__error').textContent = message;
-            input.parentNode.className = 'form__group form__group--error';
-        }
+    function setErrorFor(input, message) {
+        check = false;
+        input.parentNode.querySelector('.form__error').textContent = message;
+        input.parentNode.className = 'form__group form__group--error';
+    }
 
-        function setSuccessFor(input) {                
-            input.parentNode.className = 'form__group form__group--success';
-        }
+    function setSuccessFor(input) {
+        input.parentNode.className = 'form__group form__group--success';
+    }
 
-        return check;
+    return check;
 };
 
 export default checkInputs;
--- src/js/modules/timer.js
+++ src/js/modules/timer.js
@@ -3,12 +3,12 @@ const timer = (id, deadline) => {
     const addZero = num => num < 10 ? '0' + num : num;
 
     function getTimeRemaining(endtime) {
-        const   total = Date.parse(endtime) - Date.parse(new Date()),
-                seconds = Math.floor((total / 1000) % 60),
-                minutes = Math.floor((total / 1000 / 60) % 60),
-                hours = Math.floor((total / 1000 / 60 / 60) % 24),
-                days = Math.floor(total / 1000 / 60 / 60 / 24);
-        
+        const total = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((total / 1000) % 60),
+            minutes = Math.floor((total / 1000 / 60) % 60),
+            hours = Math.floor((total / 1000 / 60 / 60) % 24),
+            days = Math.floor(total / 1000 / 60 / 60 / 24);
+
         return {
             'total': total,
             'seconds': seconds,
@@ -19,13 +19,13 @@ const timer = (id, deadline) => {
     }
 
     function setClock(selector, endtime) {
-        const   timer = document.querySelector(selector),
-                days = timer.querySelector('#days'),
-                hours = timer.querySelector('#hours'),
-                minutes = timer.querySelector('#minutes'),
-                seconds = timer.querySelector('#seconds'),
-                timeInterval = setInterval(updateClock, 1000);
-        
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
         updateClock();
 
         function updateClock() {
@@ -36,12 +36,12 @@ const timer = (id, deadline) => {
             minutes.textContent = addZero(t.minutes);
             seconds.textContent = addZero(t.seconds);
 
-            if(t.total <= 0) {
+            if (t.total <= 0) {
                 days.textContent = '00';
                 hours.textContent = '00';
                 minutes.textContent = '00';
                 seconds.textContent = '00';
-                
+
                 clearInterval(timeInterval);
             }
         }
